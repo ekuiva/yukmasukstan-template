729a6bdf4a9b81e2770afe1642be7ad46d39e3df
Lani Asep Sutisna <laniasep.sutisna@gmail.com>
2017-11-05 14:56:32 +0700

    Update

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -10,12 +10,17 @@
         var o = e.originalEvent,
             a = o.wheelDelta || -o.detail;
         this.scrollTop += 30 * (a < 0 ? 1 : -1), e.preventDefault()
-    }), e(document).scroll(function() {
-        e(this).scrollTop() > 100 ? e(".scroll-to-top").fadeIn() : e(".scroll-to-top").fadeOut()
+    /*}), e(document).scroll(function() {
+        e(this).scrollTop() > 100 ? e(".scroll-to-top").fadeIn() : e(".scroll-to-top").fadeOut() */
     }), e('[data-toggle="tooltip"]').tooltip(), e(document).on("click", "a.scroll-to-top", function(o) {
         var a = e(this);
         e("html, body").stop().animate({
             scrollTop: e(a.attr("href")).offset().top
         }, 1e3, "easeInOutExpo"), o.preventDefault()
-    })
+    }),
+	$('#myTab a').on('click', function (e) {
+	  e.preventDefault()
+	  $(this).tab('show')
+	})
+	
 }(jQuery);
